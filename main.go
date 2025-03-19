e304091463cb1fb017ec6f70f31c07402dbc71f4
Mo <moho2010@live.ie>
2025-03-19 10:58:46 -0400

    Project Setup

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/Mo-stlyActive/Go-React-CalorieTracker/routes"
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+	router.Use(cors.Default())
+
+	router.POST("/entry/create", routes.AddEntry)
+}
